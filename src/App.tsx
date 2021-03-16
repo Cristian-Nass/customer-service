8576f664090878976c17fbc463ed6fc14c230acd
Cristian Nasirabadi <cristian.tirdad@gmail.com>
2021-03-16 13:00:38 +0100

    implemented navbar and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import User from './components/User'
+import MapComponent from './components/Map'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>start</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route  path="/user" component={User} />
+          <Route  path="/map" component={MapComponent} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
